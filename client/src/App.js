9ef10d22eee3420876568dae3b88313c52a9c883
vartanyanE <mike.vartanyan@yahoo.com>
2021-01-21 21:01:31 -0800

    remember me

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,17 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Invoice from "./components/pages/Invoice";
 import UserContext from "./context/UserContext";
+import ClickedContext from "./context/ClickedContext";
 import axios from "axios";
 
 const App = () => {
   const [userData, setUserData] = useState({
     token: undefined,
     user: undefined,
-    check: false
+    check: false,
   });
- 
+
+  const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
     const checkLoggedIn = async () => {
@@ -37,24 +39,23 @@ const App = () => {
         setUserData({
           token,
           user: userRes.data,
-          check: true
         });
-       
       }
     };
 
     checkLoggedIn();
-    
   }, []);
   return (
     <BrowserRouter>
       <UserContext.Provider value={{ userData, setUserData }}>
-        <Switch>
-          <Route exact path="/" component={!userData.check ? Login : Invoice} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/invoice" component={Invoice} />
-        </Switch>
+        <ClickedContext.Provider value={{ clicked, setClicked }}>
+          <Switch>
+            <Route exact path="/" component={!userData ? Login : Invoice} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/invoice" component={Invoice} />
+          </Switch>
+        </ClickedContext.Provider>
       </UserContext.Provider>
     </BrowserRouter>
   );
